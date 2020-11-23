b43d842f1100ba77a3bd58e98c3e0c5699a5b673
yrisska <yriss2003@gmail.com>
2020-11-23 12:45:59 +0200

    changed while func to do while in lr.cpp

--- lr.cpp
+++ lr.cpp
@@ -9,9 +9,10 @@ int main()
     cout << "Input x1,x2 and step"<<endl;
     cin >> x1 >> x2 >> h;
     x = x1;
-    while (x <= (x2 + h / 2)) {
+    do {
         y = sqrt(sin(x*M_PI/180)) + pow(M_E,x) - 3;
         cout <<setw(10)<< x<<setw(10)<<y<<endl;
         x+=h;
-    }
+    	}
+	while (x <= (x2 + h / 2))
 }
