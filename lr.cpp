8f11f14c9c434ab46256b0ac0945e1915db706fb
yrisska <yriss2003@gmail.com>
2020-11-23 12:23:11 +0200

    added lr.cpp

---
+++ lr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+int main()
+{
+    double y, x, x1, x2, h;
+    cout << "Input x1,x2 and step"<<endl;
+    cin >> x1 >> x2 >> h;
+    x = x1;
+    while (x <= (x2 + h / 2)) {
+        y = sqrt(sin(x*M_PI/180)) + pow(M_E,x) - 3;
+        cout <<setw(10)<< x<<setw(10)<<y<<endl;
+        x+=h;
+    }
+}
